4acf7598240bc6d713cd01c84df9e438580c8304
skalsait <skalsait@dyl.com>
2024-02-06 14:11:23 +0530

    --- Auto Git Commit ---

--- my.tf
+++ my.tf
@@ -1,2 +1,4 @@
 
-create a security group(web_gp) that allows internet traffic on port 80 and 22, also allow outgoing traffic on all ports
+#create a security group(web_gp) that allows internet traffic on port 80 and 22, also allow outgoing traffic on all ports
+
+resource 
