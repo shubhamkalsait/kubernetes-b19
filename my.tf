3739c82d0ce3d38bdfff6735bab413b7377860cd
skalsait <skalsait@dyl.com>
2024-02-06 14:11:43 +0530

    --- Auto Git Commit ---

--- my.tf
+++ my.tf
@@ -1,4 +1,6 @@
 
 #create a security group(web_gp) that allows internet traffic on port 80 and 22, also allow outgoing traffic on all ports
 
-resource 
+resource "aws_security_group" "my_sg" {
+    
+}
