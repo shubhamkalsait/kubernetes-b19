489adffb943a6729f5b79aea2b0986562759ef94
skalsait <skalsait@dyl.com>
2024-02-06 14:12:53 +0530

    --- Auto Git Commit ---

--- my.tf
+++ my.tf
@@ -6,6 +6,7 @@ resource "aws_security_group" "my_sg" {
     description = " allow traffic on port 80 and 22"
     ingress {
         cidr_block = "0.0.0.0/0"
-        
+        port = 80
+        to_port
     }
 }
